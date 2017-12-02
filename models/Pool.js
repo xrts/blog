66e091e1f3d2e575f2f01998614a82c272d2335d
xrts <286316299@qq.com>
2017-12-02 10:26:31 +0800

    修复了代码块手机端显示溢出和行号错误问题

--- models/Pool.js
+++ models/Pool.js
@@ -1,29 +1,3 @@
-/*
-var mysql=require('mysql');
-
-function OptPool(){
-	this.flag=true;
-	this.pool=mysql.createPool({
-		host:'localhost',
-		user:'root',
-		password:'',
-		database:'blog',
-		port:'3306'
-	});
-	this.getPool=function(){
-		if(this.flag){
-			this.pool.on('connection',function(connection){
-				connection.query('set session auto_increment_increment=1');
-				this.flag=false;
-			});
-		}
-		return this.pool;
-	}
-	
-}
-
-module.exports=OptPool;*/
-
 
 var mysql=require("mysql");
 var pool = mysql.createPool({
@@ -31,21 +5,17 @@ var pool = mysql.createPool({
     connectionLimit:2,
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'chengji',
     database: 'blog',
     port: '3306'
 });
 
 var query=function(sql,param,callback){
    
-/*    if (typeof param === 'function') {
-        console.log('sql:'+sql );
-    } else if (param !== undefined) {
-        console.log('sql:'+sql +'\nparam:'+ param);
-    }
-*/
+
     pool.getConnection(function(err,conn){
         if(err){
+            console.log(err);
             callback(err,null,null);
         }else{
              conn.query(sql,param,function(qerr,vals,fields){
